f714b4e8c32b7a928bcc42f20d21628c7edfd499
Richard Lyons <rick@frob.com.au>
2024-05-05 12:59:21 +0200

    Switch to a yellow theme (not quite there)

--- init.lua
+++ init.lua
@@ -744,6 +744,18 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 2000,
+    config = function()
+      require('gruvbox').setup {
+        pallete_overrides = {
+          light0 = '#ffffdd',
+        },
+      }
+    end,
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -755,7 +767,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'gruvbox'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -892,6 +905,8 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 })
 
 vim.opt.mouse = ''
+
+vim.o.background = 'light'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
